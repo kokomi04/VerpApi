39feecd76d2433cb5ae0453ff705913aa85b0d05
trungvt <trungvt@pixelz.com>
2019-09-12 19:09:11 +0700

    Create project

---
+++ ZTools/MigrateAndMappingApi/ClientApp/src/components/mappings/sync-api-endpoints.js
@@ -0,0 +1,36 @@
+﻿import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class SyncApiEndpoint extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    executeAction = (num) => () => {
+        console.log(num);
+        this.setState({
+            loading: true
+        })
+    }
+
+
+
+    render() {
+        return (
+            <button className="btn btn-primary" type="button" disabled={this.state.loading} onClick={this.executeAction(1)} >
+                <span className={"spinner-border spinner-border-sm"} hidden={!this.state.loading} role="status" aria-hidden="true"></span>
+                Đồng bộ data
+            </button>
+        );
+    }
+}
+
+export default connect()(SyncApiEndpoint);
