1815c7670c3c44b4e92746f929ece8a60383a206
trungvt <votrongtrung@gmail.com>
2019-11-04 17:32:47 +0700

    VW-115 CRUD customer

---
+++ Commons/Enums/StandardEnum/CustomerErrorCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VErp.Commons.Enums.StandardEnum
+{
+    [ErrorCodePrefix("CUS")]
+    public enum CustomerErrorCode
+    {
+        CustomerNotFound = 1,
+    }
+}
---
+++ Services/Master/Services.Master.Model/Customer/CustomerModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using VErp.Commons.Enums.MasterEnum;
+
+namespace VErp.Services.Master.Model.Customer
+{
+    public class CustomerModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tên đối tác")]
+        [MaxLength(128, ErrorMessage = "Tên đối tác quá dài")]
+        public string CustomerName { get; set; }
+        public EnumCustomerType CustomerTypeId { get; set; }
+        [MaxLength(128, ErrorMessage = "Địa chỉ đối tác quá dài")]
+        public string Address { get; set; }
+        [MaxLength(64, ErrorMessage = "Mã số thuế quá dài")]
+        public string TaxIdNo { get; set; }
+        [MaxLength(64, ErrorMessage = "Số điện thoại quá dài")]
+        public string PhoneNumber { get; set; }
+        [MaxLength(128, ErrorMessage = "Website quá dài")]
+        public string Website { get; set; }
+        [MaxLength(128, ErrorMessage = "Email quá dài")]
+        public string Email { get; set; }
+
+        [MaxLength(128, ErrorMessage = "Mô tả quá dài")]
+        public string Description { get; set; }
+        public bool IsActived { get; set; }
+
+        public IList<CustomerContactModel> Contacts { get; set; }
+    }
+}
