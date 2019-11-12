2d06f0173f3b29eca6d298637466448132b53a2d
An Nguyễn <haan84@gmail.com>
2019-11-12 11:16:55 +0700

    VW-183 [API] Commit : Lấy thông tin tỷ lệ chuyển đổi của đơn vị tính theo Id sản phẩm

---
+++ Services/Stock/Services.Stock.Service/ProductUnitConversion/IProductUnitConversionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using VErp.Commons.Enums.MasterEnum;
+using VErp.Infrastructure.ServiceCore.Model;
+using VErp.Services.Stock.Model.Inventory;
+
+namespace VErp.Services.Stock.Service.ProductUnitConversion
+{
+    public interface IProductUnitConversionService
+    {
+        /// <summary>
+        /// Lấy danh sách tỉ lệ chuyển đổi của các đơn vị tính phụ
+        /// </summary>
+        /// <param name="productId">Id kho</param>
+        /// <param name="page"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        Task<PageData<Infrastructure.EF.StockDB.ProductUnitConversion>> GetList(int productId, int page = 1, int size = 10);
+    }
+}
