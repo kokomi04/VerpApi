62fc8d42db2bc918e5b982202d430d2b13c3533d
An Nguyễn <haan84@gmail.com>
2020-02-22 10:11:46 +0700

    [API] PurchaseOrderDb Context

--- Infrastructure/EntityFramework/PurchaseOrderDB/PurchaseOrderDBContext.cs
+++ Infrastructure/EntityFramework/PurchaseOrderDB/PurchaseOrderDBContext.cs
@@ -15,6 +15,8 @@ namespace VErp.Infrastructure.EF.PurchaseOrderDB
         {
         }
 
+        public virtual DbSet<PurchasingRequest> PurchasingRequest { get; set; }
+        public virtual DbSet<PurchasingRequestDetail> PurchasingRequestDetail { get; set; }
         public virtual DbSet<Test> Test { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
@@ -23,6 +25,34 @@ namespace VErp.Infrastructure.EF.PurchaseOrderDB
         protected void OnModelCreated(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<PurchasingRequest>(entity =>
+            {
+                entity.Property(e => e.Content).HasMaxLength(512);
+
+                entity.Property(e => e.CreatedDatetime).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Date).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.OrderCode).HasMaxLength(128);
+
+                entity.Property(e => e.PurchasingRequestCode)
+                    .IsRequired()
+                    .HasMaxLength(128);
+
+                entity.Property(e => e.UpdatedDatetime).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<PurchasingRequestDetail>(entity =>
+            {
+                entity.Property(e => e.PurchasingRequestDetailId).ValueGeneratedNever();
+
+                entity.Property(e => e.CreatedDatetime).HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.PrimaryQuantity).HasColumnType("decimal(18, 4)");
+
+                entity.Property(e => e.UpdatedDatetime).HasDefaultValueSql("(getdate())");
+            });
 
             modelBuilder.Entity<Test>(entity =>
             {
---
+++ Infrastructure/EntityFramework/PurchaseOrderDB/PurchasingRequestDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VErp.Infrastructure.EF.PurchaseOrderDB
+{
+    public partial class PurchasingRequestDetail
+    {
+        public long PurchasingRequestDetailId { get; set; }
+        public long PurchasingRequestId { get; set; }
+        public int ProductId { get; set; }
+        public int PrimaryUnitId { get; set; }
+        public decimal PrimaryQuantity { get; set; }
+        public DateTime? CreatedDatetime { get; set; }
+        public DateTime? UpdatedDatetime { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
