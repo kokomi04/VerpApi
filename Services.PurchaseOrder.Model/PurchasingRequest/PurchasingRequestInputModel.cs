99dc24982841329dfb82edf4d2278a2d226a0c50
An Nguyễn <haan84@gmail.com>
2020-02-22 19:12:17 +0700

    [API ]VW-309 Purchasing Request

---
+++ Services.PurchaseOrder.Model/PurchasingRequest/PurchasingRequestInputModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.PurchaseOrder.Model.PurchaseRequest
+{
+    public class PurchasingRequestInputModel
+    {
+        //public long PurchasingRequestId { get; set; }
+        public string PurchasingRequestCode { get; set; }
+        public string OrderCode { get; set; }
+        public long Date { get; set; }
+        public string Content { get; set; }
+        //public bool IsApproved { get; set; }
+        //public bool IsDeleted { get; set; }
+        //public int? CreatedByUserId { get; set; }
+        //public int? UpdatedByUserId { get; set; }
+        //public DateTime? CreatedDatetime { get; set; }
+        //public DateTime? UpdatedDatetime { get; set; }
+
+        public List<PurchasingRequestDetailInputModel> DetailList { set; get; }
+    }
+}
