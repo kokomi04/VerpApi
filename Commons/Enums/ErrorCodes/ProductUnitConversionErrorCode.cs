e1544edd8bbaa2152c343ee0e159f9fcf9c4a196
trungvt <votrongtrung@gmail.com>
2020-03-02 15:31:09 +0700

    dev hotfix validate product unit conversion belong to product

--- Commons/Enums/ErrorCodes/ProductUnitConversionErrorCode.cs
+++ Commons/Enums/ErrorCodes/ProductUnitConversionErrorCode.cs
@@ -1,11 +1,19 @@
-﻿namespace VErp.Commons.Enums.StandardEnum
+﻿using System.ComponentModel;
+
+namespace VErp.Commons.Enums.StandardEnum
 {
     [ErrorCodePrefix("CONV")]
     public enum ProductUnitConversionErrorCode
     {
+        [Description("Đơn vị chuyển đổi không tìm thấy")]
         ProductUnitConversionNotFound = 1,
+        [Description("Đơn vị chuyển đổi đã tồn tại")]
         ProductSecondaryUnitAlreadyExisted = 2,
+        [Description("Không thể tính giá trị đơn vị chuyển đổi")]
         SecondaryUnitConversionError = 3,
-        PrimaryUnitConversionError = 4
+        [Description("Không thể tính giá trị đơn vị chính")]
+        PrimaryUnitConversionError = 4,
+        [Description("Đơn vị chuyển đổi không thuộc về sản phẩm")]
+        ProductUnitConversionNotBelongToProduct = 5
     }
 }
