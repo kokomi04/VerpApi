11dd25be93c37d823d86a696d93044b20b678c78
thanhluanbka <thanhluanbka@gmail.com>
2020-03-14 18:39:54 +0700

    fix SwaggerFileOperationFilter.css for upload file from swagger

--- ApiCore/Filters/SwaggerFileOperationFilter.cs
+++ ApiCore/Filters/SwaggerFileOperationFilter.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
@@ -10,64 +11,99 @@ using System.Text;
 namespace VErp.Infrastructure.ApiCore.Filters
 {
 
-    //public class SwaggerFileOperationFilter : IOperationFilter
-    //{
-    //    public void Apply(Operation operation, OperationFilterContext context)
-    //    {
-    //        var fileParams = context.MethodInfo.GetParameters()
-    //            .Where(p => p.ParameterType.FullName.Equals(typeof(Microsoft.AspNetCore.Http.IFormFile).FullName));
-    //        var isUploadFile = fileParams.Any() && fileParams.Count() == 1;
-    //        if (operation.Parameters != null)
-    //        {
-    //            var lst = new List<IParameter>();
-    //            foreach (var p in operation.Parameters)
-    //            {
-    //                if (p is NonBodyParameter)
-    //                {
-    //                    var p1 = (p as NonBodyParameter);
-    //                    if (p1.Enum != null)
-    //                    {
-    //                        for (int e = 0; e < p1.Enum.Count; e++)
-    //                        {
-    //                            p1.Enum[e] = p1.Enum[e].ToString().Split(':')[0];
-    //                        }
-    //                        p1.Type = "string";
-    //                    }
+    public class SwaggerFileOperationFilter : IOperationFilter
+    {
+        //public void Apply(Operation operation, OperationFilterContext context)
+        //{
+        //    var fileParams = context.MethodInfo.GetParameters()
+        //        .Where(p => p.ParameterType.FullName.Equals(typeof(Microsoft.AspNetCore.Http.IFormFile).FullName));
+        //    var isUploadFile = fileParams.Any() && fileParams.Count() == 1;
+        //    if (operation.Parameters != null)
+        //    {
+        //        var lst = new List<IParameter>();
+        //        foreach (var p in operation.Parameters)
+        //        {
+        //            if (p is NonBodyParameter)
+        //            {
+        //                var p1 = (p as NonBodyParameter);
+        //                if (p1.Enum != null)
+        //                {
+        //                    for (int e = 0; e < p1.Enum.Count; e++)
+        //                    {
+        //                        p1.Enum[e] = p1.Enum[e].ToString().Split(':')[0];
+        //                    }
+        //                    p1.Type = "string";
+        //                }
 
 
-    //                    if (isUploadFile)
-    //                    {
-    //                        if (p1.In != "formData")
-    //                        {
-    //                            lst.Add(p1);
-    //                        }
-    //                    }
-    //                    else
-    //                    {
-    //                        lst.Add(p1);
-    //                    }
-    //                }
-    //                else
-    //                {
-    //                    lst.Add(p);
-    //                }
-    //            }
+        //                if (isUploadFile)
+        //                {
+        //                    if (p1.In != "formData")
+        //                    {
+        //                        lst.Add(p1);
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    lst.Add(p1);
+        //                }
+        //            }
+        //            else
+        //            {
+        //                lst.Add(p);
+        //            }
+        //        }
 
-    //            if (isUploadFile)
-    //            {
-    //                foreach (var f in fileParams)
-    //                {
-    //                    lst.Add(new NonBodyParameter
-    //                    {
-    //                        Name = f.Name,
-    //                        Required = true,
-    //                        Type = "file",
-    //                        In = "formData"
-    //                    });
-    //                }
-    //            }
-    //            operation.Parameters = lst;
-    //        }
-    //    }
-    //}
+        //        if (isUploadFile)
+        //        {
+        //            foreach (var f in fileParams)
+        //            {
+        //                lst.Add(new NonBodyParameter
+        //                {
+        //                    Name = f.Name,
+        //                    Required = true,
+        //                    Type = "file",
+        //                    In = "formData"
+        //                });
+        //            }
+        //        }
+        //        operation.Parameters = lst;
+        //    }
+        //}
+
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var fileParams = context.MethodInfo.GetParameters()
+                .Where(p => p.ParameterType.FullName.Equals(typeof(Microsoft.AspNetCore.Http.IFormFile).FullName));
+            var isUploadFile = fileParams.Any() && fileParams.Count() == 1;
+            if (isUploadFile)
+            {
+                operation.RequestBody.Content.Remove("multipart/form-data");
+                if (isUploadFile)
+                {
+                    foreach (var f in fileParams)
+                    {
+                        operation.RequestBody.Content.Add("multipart/form-data", new OpenApiMediaType
+                        {
+                            Schema = new OpenApiSchema
+                            {
+                                Type = "object",
+                                Format = "binary",
+                                Properties =
+                                {
+                                    {
+                                        f.Name, new OpenApiSchema
+                                        {
+                                            Type = "string",
+                                            Format = "binary",
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    }
 }
