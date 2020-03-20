23226e724fb06165f91bd16030222b9be016dc25
thanhluanbka <thanhluanbka@gmail.com>
2020-03-20 13:24:22 +0700

    them thong tin doanh nghiep

--- Services/Master/Services.Master.Model/BusinessInfo/BusinessInfoModel.cs
+++ Services/Master/Services.Master.Model/BusinessInfo/BusinessInfoModel.cs
@@ -13,14 +13,18 @@ namespace VErp.Services.Master.Model.BusinessInfo
         [Required(ErrorMessage = "Vui lòng nhập tên người đại diện")]
         [MaxLength(128, ErrorMessage = "Tên người đại diện quá dài")]
         public string LegalRepresentative { get; set; }
-        [MaxLength(128, ErrorMessage = "Địa chỉ đối tác quá dài")]
+        [Required(ErrorMessage = "Vui lòng nhập địa chỉ doanh nghiệp")]
+        [MaxLength(128, ErrorMessage = "Địa chỉ doanh nghiệp quá dài")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập mã số thuế")]
         [MaxLength(64, ErrorMessage = "Mã số thuế quá dài")]
         public string TaxIdNo { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
         [MaxLength(32, ErrorMessage = "Số điện thoại quá dài")]
         public string PhoneNumber { get; set; }
         [MaxLength(128, ErrorMessage = "Website quá dài")]
         public string Website { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập email")]
         [MaxLength(128, ErrorMessage = "Email quá dài")]
         public string Email { get; set; }
         public int? LogoFileId { get; set; }
