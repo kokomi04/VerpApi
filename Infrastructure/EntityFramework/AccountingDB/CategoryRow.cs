14fdb0ab6f25cdf75ac16cf5651a4f864946c4aa
luanpt <luanpt@topica.edu.vn>
2020-04-15 15:48:21 +0700

    commit

--- Infrastructure/EntityFramework/AccountingDB/CategoryRow.cs
+++ Infrastructure/EntityFramework/AccountingDB/CategoryRow.cs
@@ -5,8 +5,16 @@ namespace VErp.Infrastructure.EF.AccountingDB
 {
     public partial class CategoryRow : BaseEntity
     {
+        public CategoryRow()
+        {
+            CategoryRowValues = new HashSet<CategoryRowValue>();
+        }
         public int CategoryRowId { get; set; }
         public int CategoryId { get; set; }
         public bool IsDeleted { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual ICollection<CategoryRowValue> CategoryRowValues { get; set; }
     }
 }
