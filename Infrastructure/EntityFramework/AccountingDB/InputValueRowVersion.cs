228df3d576ff06ff9bb2e80bcdac2ef9179d91ea
luanpt <luanpt@topica.edu.vn>
2020-04-17 09:42:01 +0700

    commit

--- Infrastructure/EntityFramework/AccountingDB/InputValueRowVersion.cs
+++ Infrastructure/EntityFramework/AccountingDB/InputValueRowVersion.cs
@@ -32,26 +32,6 @@ namespace VErp.Infrastructure.EF.AccountingDB
         public string Field18 { get; set; }
         public string Field19 { get; set; }
         public string Field20 { get; set; }
-        public long Field21 { get; set; }
-        public long Field22 { get; set; }
-        public long Field23 { get; set; }
-        public long Field24 { get; set; }
-        public long Field25 { get; set; }
-        public long Field26 { get; set; }
-        public long Field27 { get; set; }
-        public long Field28 { get; set; }
-        public long Field29 { get; set; }
-        public long Field30 { get; set; }
-        public long Field31 { get; set; }
-        public long Field32 { get; set; }
-        public long Field33 { get; set; }
-        public long Field34 { get; set; }
-        public long Field35 { get; set; }
-        public long Field36 { get; set; }
-        public long Field37 { get; set; }
-        public long Field38 { get; set; }
-        public long Field39 { get; set; }
-        public long Field40 { get; set; }
 
         public virtual InputValueRow InputValueRow { get; set; }
 
