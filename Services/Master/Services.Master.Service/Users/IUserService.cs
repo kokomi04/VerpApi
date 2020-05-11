6b777cf21f40f84b6bc028f98b8189c5819b25b2
trungvt <votrongtrung@gmail.com>
2020-05-11 11:51:02 +0700

    VW-546 Add GetListByUserIds api

--- Services/Master/Services.Master.Service/Users/IUserService.cs
+++ Services/Master/Services.Master.Service/Users/IUserService.cs
@@ -31,5 +31,7 @@ namespace VErp.Services.Master.Service.Users
         /// <param name="pageSize">Bản ghi trên 1 trang</param>
         /// <returns></returns>
         Task<PageData<UserInfoOutput>> GetListByModuleId(int currentUserId, int moduleId,string keyword, int pageIndex, int pageSize);
+
+        Task<IList<UserInfoOutput>> GetListByUserIds(IList<int> userIds);
     }
 }
