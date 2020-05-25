650c4c82c52bdb4fd7eedfc1d266180726e2d780
thanhluanbka <thanhluanbka@gmail.com>
2020-05-25 13:52:34 +0700

    fix filter category

--- Services/ReportConfig/Services.ReportConfig.Service/IReportConfigService.cs
+++ Services/ReportConfig/Services.ReportConfig.Service/IReportConfigService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Verp.Services.ReportConfig.Model;
+using VErp.Infrastructure.ServiceCore.Model;
 
 namespace Verp.Services.ReportConfig.Service
 {
@@ -18,5 +19,7 @@ namespace Verp.Services.ReportConfig.Service
         Task<bool> ReportTypeGroupDelete(int reportTypeGroupId);
 
         Task<IList<ReportTypeGroupList>> ReportTypeGroupList();
+
+        //Task<PageData<ReportTypeModel>> ReportTypes(string keyword, int page, int size);
     }
 }
