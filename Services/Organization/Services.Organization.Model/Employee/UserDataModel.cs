6232b01841a84603a3206168a2e8655e2036b42f
trungvt <votrongtrung@gmail.com>
2020-07-02 19:49:00 +0700

    VW-699 CRU userdata

---
+++ Services/Organization/Services.Organization.Model/Employee/UserDataModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Organization.Model.Employee
+{
+    public class UserDataModel
+    {
+        public string DataContent { get; set; }
+    }
+}
