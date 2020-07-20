3397fe15d92e509841522e7f78abb77adf9db8a2
trungvt <votrongtrung@gmail.com>
2020-07-20 11:41:44 +0700

    dev hotfix add MultiSelect as category select fields

--- Commons/Constants/AccountantConstants.cs
+++ Commons/Constants/AccountantConstants.cs
@@ -7,7 +7,7 @@ namespace VErp.Commons.Constants
 {
     public static class AccountantConstants
     {
-        public static readonly List<EnumFormType> SELECT_FORM_TYPES = new List<EnumFormType>() { EnumFormType.Select, EnumFormType.SearchTable };
+        public static readonly List<EnumFormType> SELECT_FORM_TYPES = new List<EnumFormType>() { EnumFormType.Select, EnumFormType.SearchTable, EnumFormType.MultiSelect };
         public const int INPUT_TYPE_FIELD_NUMBER = 101;
         public const int CONVERT_VALUE_TO_NUMBER_FACTOR = 100000;
         public const string INPUT_TYPE_FIELDNAME_FORMAT = "Field{0}";
