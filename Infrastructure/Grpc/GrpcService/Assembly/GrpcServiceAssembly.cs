d4d5f04fa0913f90f2a7280d77fabec978b99892
vu viet anh <anh.vv160258@sis.hust.edu.vn>
2020-08-21 17:19:20 +0700

    VW-559 Create Grpc libary proto and service

---
+++ Infrastructure/Grpc/GrpcService/Assembly/GrpcServiceAssembly.cs
@@ -0,0 +1,40 @@
+﻿using GrpcProto.Protos;
+using GrpcService.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+
+namespace GrpcService.Assembly
+{
+    public static class GrpcServiceAssembly
+    {
+        public static System.Reflection.Assembly Assembly => typeof(GrpcServiceAssembly).Assembly;
+
+        public static IApplicationBuilder UseEndpointsGrpcService(this IApplicationBuilder app)
+        {
+            app.UseEndpoints(opt =>{
+                opt.MapGrpcService<InternalActivityLogService>();
+            });
+
+            return app;
+        }
+
+        public static IServiceCollection AddCustomGrpcClient(this IServiceCollection services, Uri address)
+        {
+            services.AddGrpc(options =>{
+                    
+            });
+
+            services.AddGrpcClient<InternalActivityLog.InternalActivityLogClient>(opt =>{
+                opt.Address = address;
+            })
+                .EnableCallContextPropagation(opts => opts.SuppressContextNotFoundErrors = true);
+
+            return services;
+        }
+
+        
+    }
+}
---
+++ Infrastructure/Grpc/GrpcService/Service/InternalActivityLogService.cs
@@ -0,0 +1,36 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcProto.Protos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using VErp.Infrastructure.ApiCore.Attributes;
+using VErp.Services.Master.Service.Activity;
+    
+namespace GrpcService.Service
+{
+    public class InternalActivityLogService: InternalActivityLog.InternalActivityLogBase
+    {
+        private readonly IActivityService _activityService;
+
+        public InternalActivityLogService(IActivityService activityService)
+        {
+            _activityService = activityService;
+        }
+
+        public override Task<ActivityReponse> Log(ActivityInput request, ServerCallContext context)
+        {
+            _activityService.CreateActivityAsync(new VErp.Infrastructure.ServiceCore.Model.ActivityInput
+            {
+                UserId = request.UserId,
+                ActionId = (VErp.Commons.Enums.MasterEnum.EnumAction)request.ActionId,
+                ObjectTypeId = (VErp.Commons.Enums.MasterEnum.EnumObjectType)request.ObjectTypeId,
+                MessageTypeId = (VErp.Commons.Enums.EnumMessageType)request.MessageTypeId,
+                ObjectId = request.ObjectId,
+                Message = request.Message,
+                Data = request.Data
+            });
+            return  Task.FromResult(new ActivityReponse { IsSuccess = true }) ;
+        }
+    }
+}
