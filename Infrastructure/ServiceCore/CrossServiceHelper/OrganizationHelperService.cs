a28d3d57ff2789fca0d7f28d9afe5d1826f64d8e
vu viet anh <anh.vv160258@sis.hust.edu.vn>
2020-09-05 11:36:27 +0700

    VW-560 Convert CustomGenCode and Organization methods to Grpc

--- Infrastructure/ServiceCore/CrossServiceHelper/OrganizationHelperService.cs
+++ Infrastructure/ServiceCore/CrossServiceHelper/OrganizationHelperService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using GrpcProto.Protos;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
@@ -24,20 +25,62 @@ namespace VErp.Infrastructure.ServiceCore.CrossServiceHelper
         private readonly IHttpCrossService _httpCrossService;
         private readonly AppSetting _appSetting;
         private readonly ILogger _logger;
-        public OrganizationHelperService(IHttpCrossService httpCrossService, IOptions<AppSetting> appSetting, ILogger<ProductHelperService> logger)
+        private readonly OrganizationProvider.OrganizationProviderClient _organizationClient;
+        public OrganizationHelperService(IHttpCrossService httpCrossService,
+            IOptions<AppSetting> appSetting,
+            ILogger<ProductHelperService> logger,
+            OrganizationProvider.OrganizationProviderClient organizationClient)
         {
             _httpCrossService = httpCrossService;
             _appSetting = appSetting.Value;
             _logger = logger;
+            _organizationClient = organizationClient;
         }
 
         public async Task<BaseCustomerModel> CustomerInfo(int customerId)
         {
+            if (_appSetting.GrpcInternal?.Address?.Contains("https") == true)
+            {
+                var result = await _organizationClient.CustomerInfoAsync(new CustomerInfoRequest{ CustomerId = customerId });
+                return new BaseCustomerModel
+                {
+                    Address = result.Address,
+                    CustomerCode = result.CustomerCode,
+                    CustomerName = result.CustomerName,
+                    CustomerStatusId = (Commons.Enums.MasterEnum.EnumCustomerStatus)result.CustomerStatusId,
+                    CustomerTypeId = (Commons.Enums.MasterEnum.EnumCustomerType)result.CustomerTypeId,
+                    DebtDays = result.DebtDays,
+                    Description = result.Description,
+                    Email = result.Email,
+                    Identify = result.Identify,
+                    IsActived = result.IsActived,
+                    LegalRepresentative = result.LegalRepresentative,
+                    PhoneNumber = result.PhoneNumber,
+                    TaxIdNo = result.TaxIdNo,
+                    Website = result.Website
+                    
+                };
+            }
             return await _httpCrossService.Get<BaseCustomerModel>($"api/internal/InternalCustomer/{customerId}");
         }
 
         public async Task<BusinessInfoModel> BusinessInfo()
         {
+            if (_appSetting.GrpcInternal?.Address?.Contains("https") == true)
+            {
+                var result = await _organizationClient.BusinessInfoAsync(new Google.Protobuf.WellKnownTypes.Empty());
+                return new BusinessInfoModel
+                {
+                    Website = result.Website,
+                    Address = result.Address,
+                    CompanyName = result.CompanyName,
+                    Email = result.Email,
+                    LegalRepresentative = result.LegalRepresentative,
+                    LogoFileId = result.LogoFileId,
+                    PhoneNumber = result.PhoneNumber,
+                    TaxIdNo = result.TaxIdNo
+                };
+            }
             return await _httpCrossService.Get<BusinessInfoModel>($"api/internal/InternalBussiness/businessInfo");
         }
 
--- Services.Grpc.Service/Service/InternalProductService.cs
+++ Services.Grpc.Service/Service/InternalProductService.cs
@@ -54,7 +54,7 @@ namespace VErp.Services.Grpc.Service
                         ProductTypeId = (int)product.ProductTypeId,
                         ProductCateId = product.ProductCateId,
                         BarcodeConfigId = (int)product.BarcodeConfigId,
-                        BarcodeStandardId = (VErp.Grpc.Protos.Enums.EnumBarcodeStandard)product.BarcodeStandardId,
+                        BarcodeStandardId = (int)product.BarcodeStandardId,
                         Barcode = product.Barcode,
                         UnitId = product.UnitId,
                         EstimatePrice = product.EstimatePrice,
@@ -66,12 +66,12 @@ namespace VErp.Services.Grpc.Service
                         } : null,
                         StockInfo = productStockInfo != null ? new ProductModelStock()
                         {
-                            StockOutputRuleId = (VErp.Grpc.Protos.Enums.EnumStockOutputRule)productStockInfo.StockOutputRuleId,
+                            StockOutputRuleId = (int)productStockInfo.StockOutputRuleId,
                             AmountWarningMin = (long)productStockInfo.AmountWarningMin,
                             AmountWarningMax = (long)productStockInfo.AmountWarningMax,
-                            TimeWarningTimeTypeId = (VErp.Grpc.Protos.Enums.EnumTimeType)productStockInfo.TimeWarningTimeTypeId,
+                            TimeWarningTimeTypeId = (int)productStockInfo.TimeWarningTimeTypeId,
                             TimeWarningAmount = (double)productStockInfo.TimeWarningAmount,
-                            ExpireTimeTypeId = (VErp.Grpc.Protos.Enums.EnumTimeType)productStockInfo.ExpireTimeTypeId,
+                            ExpireTimeTypeId = (int)productStockInfo.ExpireTimeTypeId,
                             ExpireTimeAmount = (double)productStockInfo.ExpireTimeAmount,
                             DescriptionToStock = productStockInfo.DescriptionToStock,
 
@@ -93,6 +93,97 @@ namespace VErp.Services.Grpc.Service
                     await responseStream.WriteAsync(productData);
                 }
                 
+            }
+        }
+
+        public async override Task<ValidateProductResponses> ValidateProductUnitConversions(ValidateProductRequest request, ServerCallContext context)
+        {
+            var productIds = request.ProductUnitConvertsionProduct.Values;
+            var productUnitConversionIds = request.ProductUnitConvertsionProduct.Keys;
+
+            var productUnitConversions = (await _stockContext.ProductUnitConversion
+                .Where(c => productIds.Contains(c.ProductId) && productUnitConversionIds.Contains(c.ProductUnitConversionId))
+                .ToListAsync())
+                .ToDictionary(c => c.ProductUnitConversionId, c => c.ProductId);
+
+            foreach (var item in request.ProductUnitConvertsionProduct)
+            {
+                if (!productUnitConversions.ContainsKey(item.Key) || productUnitConversions[item.Key] != item.Value) 
+                    return await Task.FromResult(new ValidateProductResponses { Result = false });
+            }
+
+            return await Task.FromResult(new ValidateProductResponses { Result = true }); ;
+        }
+
+        public async override Task GetListByCodeAndInternalNames(GetListByCodeAndInternalNamesRequest request, IServerStreamWriter<ProductModel> responseStream, ServerCallContext context)
+        {
+            var productCodes = request.ProductCodes;
+            var productInternalNames = request.ProductInternalNames;
+
+            var products = await _stockContext.Product.AsNoTracking().Where(p => productCodes.Contains(p.ProductCode) || productInternalNames.Contains(p.ProductInternalName)).ToListAsync();
+
+            var productIds = products.Select(d => d.ProductId).ToList();
+
+            var productExtraData = await _stockContext.ProductExtraInfo.AsNoTracking().Where(p => productIds.Contains(p.ProductId)).ToListAsync();
+            var productStockInfoData = await _stockContext.ProductStockInfo.AsNoTracking().Where(p => productIds.Contains(p.ProductId)).ToListAsync();
+            var stockValidationData = await _stockContext.ProductStockValidation.AsNoTracking().Where(p => productIds.Contains(p.ProductId)).ToListAsync();
+            var unitConverionData = await _stockContext.ProductUnitConversion.AsNoTracking().Where(p => productIds.Contains(p.ProductId)).ToListAsync();
+
+            foreach (var product in products)
+            {
+                var productExtra = await _stockContext.ProductExtraInfo.AsNoTracking().FirstOrDefaultAsync(p => p.ProductId == product.ProductId);
+                var productStockInfo = await _stockContext.ProductStockInfo.AsNoTracking().FirstOrDefaultAsync(p => p.ProductId == product.ProductId);
+                var stockValidations = await _stockContext.ProductStockValidation.AsNoTracking().Where(p => p.ProductId == product.ProductId).ToListAsync();
+                var unitConverions = await _stockContext.ProductUnitConversion.AsNoTracking().Where(p => p.ProductId == product.ProductId).ToListAsync();
+
+                var productData = new VErp.Grpc.Protos.ProductModel()
+                {
+                    ProductId = product.ProductId,
+                    ProductCode = product.ProductCode,
+                    ProductName = product.ProductName,
+                    IsCanBuy = product.IsCanBuy,
+                    IsCanSell = product.IsCanSell,
+                    MainImageFileId = (long)product.MainImageFileId,
+                    ProductTypeId = (int)product.ProductTypeId,
+                    ProductCateId = product.ProductCateId,
+                    BarcodeConfigId = (int)product.BarcodeConfigId,
+                    BarcodeStandardId = (int)product.BarcodeStandardId,
+                    Barcode = product.Barcode,
+                    UnitId = product.UnitId,
+                    EstimatePrice = product.EstimatePrice,
+
+                    Extra = productExtra != null ? new ProductModelExtra()
+                    {
+                        Specification = productExtra.Specification,
+                        Description = productExtra.Description
+                    } : null,
+                    StockInfo = productStockInfo != null ? new ProductModelStock()
+                    {
+                        StockOutputRuleId = (int)productStockInfo.StockOutputRuleId,
+                        AmountWarningMin = (long)productStockInfo.AmountWarningMin,
+                        AmountWarningMax = (long)productStockInfo.AmountWarningMax,
+                        TimeWarningTimeTypeId = (int)productStockInfo.TimeWarningTimeTypeId,
+                        TimeWarningAmount = (double)productStockInfo.TimeWarningAmount,
+                        ExpireTimeTypeId = (int)productStockInfo.ExpireTimeTypeId,
+                        ExpireTimeAmount = (double)productStockInfo.ExpireTimeAmount,
+                        DescriptionToStock = productStockInfo.DescriptionToStock,
+
+                    } : null
+                };
+
+                productData.StockInfo.StockIds.AddRange(stockValidations?.Select(s => s.StockId).ToList());
+                productData.StockInfo.UnitConversions.AddRange(unitConverions?.Select(c => new ProductModelUnitConversion()
+                {
+                    ProductUnitConversionId = c.ProductUnitConversionId,
+                    ProductUnitConversionName = c.ProductUnitConversionName,
+                    SecondaryUnitId = c.SecondaryUnitId,
+                    IsDefault = c.IsDefault,
+                    IsFreeStyle = c.IsFreeStyle ?? false,
+                    FactorExpression = c.FactorExpression,
+                    ConversionDescription = c.ConversionDescription
+                }).ToList());
+
+                await responseStream.WriteAsync(productData);
             }
         }
     }
