877b2c0da3551c7d81462f000c89b7f7c6b9e593
thanhluanbka <thanhluanbka@gmail.com>
2020-09-18 14:32:37 +0700

    VW-748 Add CalcBillService

---
+++ Services/Accountancy/Services.Accountancy.Service/Input/ICalcBillService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VErp.Commons.Enums.AccountantEnum;
+using VErp.Commons.GlobalObject;
+using VErp.Services.Accountancy.Model.Input;
+
+namespace VErp.Services.Accountancy.Service.Input
+{
+    public interface ICalcBillService
+    {
+        Task<ICollection<NonCamelCaseDictionary>> CalcFixExchangeRate(long toDate, int currency, int exchangeRate);
+
+        Task<bool> CheckExistedFixExchangeRate(long fromDate, long toDate);
+
+        Task<bool> DeletedFixExchangeRate(long fromDate, long toDate);
+
+        Task<ICollection<NonCamelCaseDictionary>> CalcCostTransfer(long toDate, EnumCostTransfer type, bool byDepartment, bool byCustomer, bool byFixedAsset, bool byExpenseItem, bool byFactory, bool byProduct, bool byStock);
+
+        ICollection<CostTransferTypeModel> GetCostTransferTypes();
+
+        Task<bool> CheckExistedCostTransfer(EnumCostTransfer type, long fromDate, long toDate);
+
+        Task<bool> DeletedCostTransfer(EnumCostTransfer type, long fromDate, long toDate);
+
+        Task<ICollection<NonCamelCaseDictionary>> CalcCostTransferBalanceZero(long toDate);
+
+        Task<bool> CheckExistedCostTransferBalanceZero(long fromDate, long toDate);
+
+        Task<bool> DeletedCostTransferBalanceZero(long fromDate, long toDate);
+    }
+}
