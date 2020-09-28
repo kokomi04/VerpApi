faefe1a5f8b6ac43e78f1b740fa01c23ecb18a0e
trungvt <votrongtrung@gmail.com>
2020-09-28 10:09:04 +0700

    VW-835 Replace AccoutantConfig And InventoryConfig by DataConfig

---
+++ Services/Master/Services.Master.Service/Config/IDataConfigService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VErp.Services.Master.Model.Config;
+
+namespace VErp.Services.Master.Service.Config
+{
+    public interface IDataConfigService
+    {
+        Task<DataConfigModel> GetConfig();
+
+        Task<bool> UpdateConfig(DataConfigModel req);
+    }
+}
