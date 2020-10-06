2543e40df5f523264ad8efddf79c5caa164ac9fa
Tuan Nguyen <minhtuan.titzero@gmail.com>
2020-10-06 15:39:31 +0700

    VW-825 CRUD Category permission

---
+++ Services/Master/Services.Master.Model/RolePermission/CategoryPermissionModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VErp.Services.Master.Model.RolePermission
+{
+    public class CategoryPermissionModel
+    {
+        public int CategoryId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
