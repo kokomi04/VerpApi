5b2139d9d258449a4636aff36010ccdc5fd2a52d
Tuan Nguyen <minhtuan.titzero@gmail.com>
2020-10-14 14:47:32 +0700

    VW-880 Import excel employee

---
+++ Services/Master/Services.Master.Model/Users/UserImportModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using VErp.Commons.Enums.MasterEnum;
+
+namespace VErp.Services.Master.Model.Users
+{
+    public class UserImportModel
+    {
+        [Display(Name ="Mã Nhân viên")]
+        public string EmployeeCode { get; set; }
+
+        [Display(Name = "Họ và tên")]
+        [Required(ErrorMessage = "Vui lòng nhập họ tên")]
+        public string FullName { get; set; }
+
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Số điện thoại")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Địa chỉ")]
+        public string Address { get; set; }
+
+        [Display(Name = "Giới tính")]
+        public EnumGender? GenderId { get; set; }
+
+        [Display(Name = "Tên đăng nhập")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+
+        [Display(Name = "Trạng thái")]
+        public EnumUserStatus UserStatusId { get; set; }
+
+        [Display(Name = "Nhóm quyền")]
+        public int? RoleId { get; set; }
+    }
+}
