e6e0185c6131eaf31874afcedcc1a1a6bc7cba37
trungvt <votrongtrung@gmail.com>
2020-10-22 14:47:55 +0700

    VW-887 add debt and loan info config to customer

--- Services/Organization/Services.Organization.Model/Customer/CustomerListOutput.cs
+++ Services/Organization/Services.Organization.Model/Customer/CustomerListOutput.cs
@@ -16,8 +16,18 @@ namespace VErp.Services.Organization.Model.Customer
         public string PhoneNumber { get; set; }
         public string Website { get; set; }
         public string Email { get; set; }
-        public string Identify { get; set; }
-        public int? DebtDays { get; set; }
+        public string Identify { get; set; }        
         public EnumCustomerStatus CustomerStatusId { get; set; }
+
+        public int? DebtDays { get; set; }
+        public decimal? DebtLimitation { get; set; }
+        public EnumBeginingType DebtBeginingTypeId { get; set; }
+        public int? DebtManagerUserId { get; set; }
+
+        public int? LoanDays { get; set; }
+        public decimal? LoanLimitation { get; set; }
+        public EnumBeginingType LoanBeginingTypeId { get; set; }
+        public int? LoanManagerUserId { get; set; }
+
     }
 }
