db5ae8271a2f6302c1510c2ca639e6ed883d599a
thanhluanbka <thanhluanbka@gmail.com>
2020-11-03 17:16:24 +0700

    VW-926 CRUD BOM

--- Services/Stock/Services.Stock.Model/Product/ProductBomOutput.cs
+++ Services/Stock/Services.Stock.Model/Product/ProductBomOutput.cs
@@ -8,25 +8,17 @@ namespace VErp.Services.Stock.Model.Product
     {
         public long ProductBomId { get; set; }
         public int? Level { get; set; }
-
-        //public int RootProductId { get; set; }
-
         public int? ProductId { get; set; }
         public int? ParentProductId { get; set; }
 
         public string ProductCode { get; set; }
 
-        public string ProductName { get; set; }        
-
-        public string ProductCateName { get; set; }
-
+        public string ProductName { get; set; }     
         public string ProductSpecification { get; set; }
 
         public decimal? Quantity { get; set; }
         public decimal? Wastage { get; set; }
+        public decimal? Total { get; set; }
         public string Description { get; set; }
-        public long CreatedDatetimeUtc { get; set; }
-        public long UpdatedDatetimeUtc { get; set; }
-        
     }
 }
--- WebApis/VErpApi/Controllers/Stock/Products/ProductBomController.cs
+++ WebApis/VErpApi/Controllers/Stock/Products/ProductBomController.cs
@@ -23,35 +23,13 @@ namespace VErpApi.Controllers.Stock.Products
             _productBomService = productBomService;
         }
 
-        /// <summary>
-        /// Lấy thông tin 1 bom theo mã
-        /// </summary>
-        /// <param name="productBomId">Id của 1 bom</param>
-        /// <returns></returns>
         [HttpGet]
         [Route("")]
-        public async Task<ProductBomOutput> Get([FromQuery] int productBomId)
+        public async Task<IList<ProductBomOutput>> GetBOM([FromQuery] int productBomId)
         {
-            return await _productBomService.Get(productBomId);
+            return await _productBomService.GetBOM(productBomId);
         }
-
-        /// <summary>
-        /// Lấy danh sách (toàn bộ) vật tư và chi tiết cấu thành (Bom) của sản phẩm theo ProductId
-        /// </summary>
-        /// <param name="productId">Mã Id SPVTHH</param>
-        /// <returns></returns>
-        [HttpGet]
-        [Route("GetAll")]
-        public async Task<PageData<ProductBomOutput>> GetAll([FromQuery] int productId)
-        {
-            return await _productBomService.GetAll(productId);
-        }
-
-        /// <summary>
-        /// Thêm mới thông tin vào ProductBom
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
+        
         [HttpPost]
         [Route("")]
         public async Task<long> Add([FromBody] ProductBomInput model)
@@ -59,12 +37,6 @@ namespace VErpApi.Controllers.Stock.Products
             return await _productBomService.Add(model);
         }
 
-        /// <summary>
-        /// Cập nhật thông tin 
-        /// </summary>
-        /// <param name="productBomId">Id của productBom</param>
-        /// <param name="model">input productBom model </param>
-        /// <returns></returns>
         [HttpPut]
         [Route("")]
         public async Task<bool> Update([FromQuery] long productBomId ,[FromBody] ProductBomInput model)
@@ -72,12 +44,6 @@ namespace VErpApi.Controllers.Stock.Products
             return await _productBomService.Update(productBomId, model);
         }
 
-        /// <summary>
-        /// Xoá thông tin productBom của sản phẩm
-        /// </summary>
-        /// <param name="productBomId">Id của bom</param>
-        /// <param name="rootProductId">Id của sản phẩm có chứa bom đó</param>
-        /// <returns></returns>
         [HttpDelete]
         [Route("")]
         public async Task<bool> Delete([FromQuery] long productBomId,[FromQuery] int rootProductId)
