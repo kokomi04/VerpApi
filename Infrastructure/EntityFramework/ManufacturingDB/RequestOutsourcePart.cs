685df020dace4483b66332e7aa1d989fb261d2f8
Tuan Nguyen <minhtuan.titzero@gmail.com>
2020-11-06 16:55:14 +0700

    VW-934 CRUD Gia cong chi tiet

--- Infrastructure/EntityFramework/ManufacturingDB/RequestOutsourcePart.cs
+++ Infrastructure/EntityFramework/ManufacturingDB/RequestOutsourcePart.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 namespace VErp.Infrastructure.EF.ManufacturingDB
 {
-    public partial class OutsourceComposition
+    public partial class RequestOutsourcePart
     {
-        public int OutsourceCompositionId { get; set; }
-        public int ProductionOrderId { get; set; }
-        public int ProductId { get; set; }
+        public int RequestOutsourcePartId { get; set; }
+        public int ProductionOrderDetailId { get; set; }
+        public int ProductInStepId { get; set; }
         public int Quantity { get; set; }
         public DateTime CreatedDatetimeUtc { get; set; }
         public int CreatedByUserId { get; set; }
@@ -15,5 +15,10 @@ namespace VErp.Infrastructure.EF.ManufacturingDB
         public DateTime UpdatedDatetimeUtc { get; set; }
         public int UpdatedByUserId { get; set; }
         public DateTime? DeletedDatetimeUtc { get; set; }
+        public string RequestOrder { get; set; }
+        public int UnitId { get; set; }
+
+        public virtual ProductInStep ProductInStep { get; set; }
+        public virtual ProductionOrderDetail ProductionOrderDetail { get; set; }
     }
 }
---
+++ WebApis/VErpApi/Controllers/Manufacturing/Outsource/RequestOutsourceController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VErp.Infrastructure.ServiceCore.Model;
+using VErp.Services.Manafacturing.Model.Outsource.RequestPart;
+using VErp.Services.Manafacturing.Service.Outsource;
+
+namespace VErpApi.Controllers.Manufacturing.Outsource
+{
+    [Route("api/requestOutsource")]
+    [ApiController]
+    public class RequestOutsourceController : ControllerBase
+    {
+        private readonly IRequestOutsourcePartService _requestPartService;
+
+        public RequestOutsourceController(IRequestOutsourcePartService requestOutsourcePartService)
+        {
+            _requestPartService = requestOutsourcePartService;
+        }
+
+        [HttpGet]
+        [Route("parts")]
+        public async Task<PageData<RequestOutsourcePartModel>> GetListRequestPart([FromQuery] string keyWord, [FromQuery] int page, [FromQuery] int size)
+        {
+            return await _requestPartService.GetListRequest(keyWord, page, size);
+        }
+        [HttpGet]
+        [Route("parts/{requestPartId}")]
+        public async Task<RequestOutsourcePartModel> GetRequestPartById([FromRoute] int requestPartId)
+        {
+            return await _requestPartService.GetRequestById(requestPartId);
+        }
+        [HttpPost]
+        [Route("parts")]
+        public async Task<int> CreateRequestPart([FromBody] RequestOutsourcePartModel req)
+        {
+            return await _requestPartService.CreateRequest(req);
+        }
+        [HttpPut]
+        [Route("parts/{requestPartId}")]
+        public async Task<bool> UpdateRequestPart([FromRoute] int requestPartId, [FromBody] RequestOutsourcePartModel req)
+        {
+            return await _requestPartService.UpdateRequest(requestPartId, req);
+        }
+        [HttpGet]
+        [Route("parts/{requestPartId}")]
+        public async Task<bool> DeleteRequestPart([FromRoute] int requestPartId)
+        {
+            return await _requestPartService.DeleteRequest(requestPartId);
+        }
+
+    }
+}
