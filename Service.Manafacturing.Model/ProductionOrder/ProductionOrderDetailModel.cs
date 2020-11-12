5d40380d5c6dd0a95a2fbc6742f14aad448a8db3
thanhluanbka <thanhluanbka@gmail.com>
2020-11-12 10:19:49 +0700

    VW-931 Add api get product info by order id

--- Service.Manafacturing.Model/ProductionOrder/ProductionOrderDetailModel.cs
+++ Service.Manafacturing.Model/ProductionOrder/ProductionOrderDetailModel.cs
@@ -22,12 +22,14 @@ namespace VErp.Services.Manafacturing.Model.ProductionOrder
 
     public class ProductionOrderExtraInfo
     {
-        public int ProductionOrderDetailId { get; set; }
+        public int? ProductionOrderDetailId { get; set; }
         public decimal? OrderQuantity { get; set; }
         public int? OrderedQuantity { get; set; }
         public string PartnerId { get; set; }
         public string PartnerCode { get; set; }
         public string PartnerName { get; set; }
         public string OrderCode { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
     }
 }
