e754a46013e1b1a6bbcb3042da5ebdde9d68db98
thanhluanbka <thanhluanbka@gmail.com>
2020-12-04 15:47:10 +0700

    VW-1003 get production inventory requirements

---
+++ Service.Manafacturing.Model/ProductionHandover/ProductionInventoryRequirementModel.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VErp.Commons.Enums.Manafacturing;
+using VErp.Commons.Enums.MasterEnum;
+using VErp.Commons.GlobalObject;
+using VErp.Commons.Library;
+
+namespace VErp.Services.Manafacturing.Model.ProductionHandover
+{
+    public class ProductionInventoryRequirementBaseModel
+    {
+        public long InventoryRequirementDetailId { get; set; }
+        public int ProductId { get; set; }
+        public int CreatedByUserId { get; set; }
+        public decimal RequirementQuantity { get; set; }
+        public decimal? ActualQuantity { get; set; }
+        public int? AssignStockId { get; set; }
+        public int StockName { get; set; }
+    }
+
+    public class ProductionInventoryRequirementModel : ProductionInventoryRequirementBaseModel, IMapFrom<ProductionInventoryRequirementEntity>
+    {
+        public int Status { get; set; }
+        public long CreatedDatetimeUtc { get; set; }
+        public EnumInventoryType InventoryTypeId { get; set; }
+
+        public virtual void Mapping(Profile profile)
+        {
+            profile.CreateMap<ProductionInventoryRequirementEntity, ProductionInventoryRequirementModel>()
+                .ForMember(m => m.InventoryTypeId, v => v.MapFrom(m => (EnumInventoryType)m.InventoryTypeId))
+                .ForMember(m => m.Status, v => v.MapFrom(m => (EnumProductionInventoryRequirementStatus)m.Status))
+                .ForMember(m => m.CreatedDatetimeUtc, v => v.MapFrom(m => m.CreatedDatetimeUtc.GetUnix()));
+        }
+    }
+
+    public class ProductionInventoryRequirementEntity : ProductionInventoryRequirementBaseModel
+    {
+        public int Status { get; set; }
+        public DateTime CreatedDatetimeUtc { get; set; }
+        public int InventoryTypeId { get; set; }
+    }
+}
