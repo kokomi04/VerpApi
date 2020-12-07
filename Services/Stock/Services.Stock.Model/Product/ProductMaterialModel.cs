24d16f9cbf4c730ce47a7e28acc390f7c7f65451
thanhluanbka <thanhluanbka@gmail.com>
2020-12-07 10:06:59 +0700

    VW-1003 Fix model ProductionMaterial

--- Services/Stock/Services.Stock.Model/Product/ProductMaterialModel.cs
+++ Services/Stock/Services.Stock.Model/Product/ProductMaterialModel.cs
@@ -4,16 +4,16 @@ using System.Text;
 using VErp.Commons.GlobalObject;
 using VErp.Infrastructure.EF.StockDB;
 using System.ComponentModel.DataAnnotations;
+using AutoMapper;
 
 namespace VErp.Services.Stock.Model.Product
 {
-    public class ProductMaterialModel : IMapFrom<ProductMaterial>
+    public class ProductMaterialModel
     {
         public int ProductMaterialId { get; set; }
         [Required(ErrorMessage = "Vui lòng nhập sản phẩm chính")]
         public int RootProductId { get; set; }
-        [Required(ErrorMessage = "Vui lòng nhập đường dẫn tới nguyên vật liệu")]
-        public string PathProductIds { get; set; }
+        public int[] PathProductIds { get; set; }
         [Required(ErrorMessage = "Vui lòng chọn sản phẩm là nguyên vật liệu")]
         public int ProductId { get; set; }
     }
