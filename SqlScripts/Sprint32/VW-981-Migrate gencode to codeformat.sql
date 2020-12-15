40a86c3710e52f3d235d49e82e7e43d58ebfd5eb
trungvt <votrongtrung@gmail.com>
2020-12-15 09:55:34 +0700

    hotfix add migrate last value

--- SqlScripts/Sprint32/VW-981-Migrate gencode to codeformat.sql
+++ SqlScripts/Sprint32/VW-981-Migrate gencode to codeformat.sql
@@ -1,3 +1,14 @@
 USE MasterDB
 GO
 UPDATE CustomGenCode SET CodeFormat = CONCAT(Prefix,'%S_NUMBER%',Suffix)
+GO
+INSERT INTO dbo.CustomGenCodeValue
+(
+    CustomGenCodeId,
+    BaseValue,
+    LastValue,
+    LastCode,
+    TempValue,
+    TempCode
+)
+SELECT CustomGenCodeId, '', LastValue, LastCode, TempValue, TempCode FROM dbo.CustomGenCode
