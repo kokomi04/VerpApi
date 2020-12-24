16d44acb7de2fc3cca346a2ad5503f837641f3e9
thanhluanbka <thanhluanbka@gmail.com>
2020-12-24 16:50:02 +0700

    VW-1016 Fix calc completed quantity

---
+++ Service.Manafacturing.Model/Report/ProcessingScheduleListModel.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VErp.Commons.GlobalObject;
+using VErp.Infrastructure.EF.ManufacturingDB;
+using VErp.Commons.Enums.MasterEnum;
+using static VErp.Commons.Enums.Manafacturing.EnumProductionProcess;
+using VErp.Services.Manafacturing.Model.Step;
+
+namespace VErp.Services.Manafacturing.Model.Report
+{
+    public class ProcessingScheduleListModel
+    {
+        public long ProductionScheduleId { get; set; }
+        public long ScheduleTurnId { get; set; }
+
+        public string ProductTitle { get; set; }
+        public long StartDate { get; set; }
+        public long EndDate { get; set; }
+
+        public IList<StepModel> Steps { get; set; }
+
+        public ProcessingScheduleListModel()
+        {
+            Steps = new List<StepModel>();
+        }
+    }
+}
