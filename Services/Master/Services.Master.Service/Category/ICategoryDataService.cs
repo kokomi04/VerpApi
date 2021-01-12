0a7c602b1fcc182c1cd6ca578a2a4a0227d2d82b
thanhluanbka <thanhluanbka@gmail.com>
2021-01-12 15:49:47 +0700

    VW-1016 Include sort for get categories api

--- Services/Master/Services.Master.Service/Category/ICategoryDataService.cs
+++ Services/Master/Services.Master.Service/Category/ICategoryDataService.cs
@@ -11,7 +11,7 @@ namespace VErp.Services.Master.Service.Category
 {
     public interface ICategoryDataService
     {
-        Task<PageData<NonCamelCaseDictionary>> GetCategoryRows(int categoryId, string keyword, string filters, string extraFilter, ExtraFilterParam[] extraFilterParams, int page, int size);
+        Task<PageData<NonCamelCaseDictionary>> GetCategoryRows(int categoryId, string keyword, string filters, string extraFilter, ExtraFilterParam[] extraFilterParams, int page, int size, string orderBy, bool asc);
 
         Task<NonCamelCaseDictionary> GetCategoryRow(int categoryId, int fId);
 
