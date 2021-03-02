d446c9c02ddb0659e84b541698913089a7517ebc
Tuan Nguyen <minhtuan.titzero@gmail.com>
2021-03-02 16:45:29 +0700

    VW-1204 API productionStep collection

---
+++ Infrastructure/EntityFramework/ManufacturingDB/ProductionStepCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VErp.Infrastructure.EF.ManufacturingDB
+{
+    public partial class ProductionStepCollection
+    {
+        public long ProductionStepCollectionId { get; set; }
+        public string Title { get; set; }
+        public int Frequence { get; set; }
+        public string Collections { get; set; }
+        public DateTime UpdatedDatetimeUtc { get; set; }
+        public int UpdatedByUserId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
