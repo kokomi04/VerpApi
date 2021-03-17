b2d9e78f88356ec70f207cf4cb791558b5619564
Tuan Nguyen <minhtuan.titzero@gmail.com>
2021-03-17 14:24:44 +0700

    VW-1241 Bug/Fix sua loi copy productSemi

--- Services.Manafacturing.Service/ProductionProcess/Implement/ProductionProcessService.cs
+++ Services.Manafacturing.Service/ProductionProcess/Implement/ProductionProcessService.cs
@@ -24,6 +24,7 @@ using VErp.Services.Manafacturing.Model.ProductionProcess;
 using VErp.Services.Manafacturing.Model.ProductionStep;
 using VErp.Services.Manafacturing.Service.Outsource;
 using static VErp.Commons.Enums.Manafacturing.EnumProductionProcess;
+using ProductSemiEnity =  VErp.Infrastructure.EF.ManufacturingDB.ProductSemi;
 
 namespace VErp.Services.Manafacturing.Service.ProductionProcess.Implement
 {
@@ -1373,13 +1374,20 @@ namespace VErp.Services.Manafacturing.Service.ProductionProcess.Implement
                         }
                         else
                         {
-                            p.ProductSemiId = 0;
-                            p.ContainerId = toContainerId;
+                            var entityProductSemi = new ProductSemiEnity
+                            {
+                                ContainerId = toContainerId,
+                                ContainerTypeId = p.ContainerTypeId,
+                                Conversion = p.Conversion,
+                                Title = p.Title,
+                                UnitId = p.UnitId,
+                            };
 
-                            _manufacturingDBContext.ProductSemi.Add(p);
+                            _manufacturingDBContext.ProductSemi.Add(entityProductSemi);
                             _manufacturingDBContext.SaveChanges();
 
-                            x.ObjectId = p.ProductSemiId;
+                            x.ObjectId = entityProductSemi.ProductSemiId;
+                            lsProductionSemiFinal.Add(entityProductSemi);
                         }
                         
                     }
