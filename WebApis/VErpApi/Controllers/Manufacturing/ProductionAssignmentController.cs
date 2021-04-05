d22e2a580ecda85a073bcdb1f028e2b12b57d231
thanhluanbka <thanhluanbka@gmail.com>
2021-04-05 15:07:35 +0700

    VW-1233 Fix router

--- WebApis/VErpApi/Controllers/Manufacturing/ProductionAssignmentController.cs
+++ WebApis/VErpApi/Controllers/Manufacturing/ProductionAssignmentController.cs
@@ -31,29 +31,29 @@ namespace VErpApi.Controllers.Manufacturing
         }
 
         [HttpGet]
-        [Route("{productionOrderId}")]
+        [Route("productionOrder/{productionOrderId}")]
         public async Task<IList<ProductionAssignmentModel>> GetProductionAssignments([FromRoute] long productionOrderId)
         {
             return await _productionAssignmentService.GetProductionAssignments(productionOrderId);
         }
 
         [HttpGet]
-        [Route("{productionOrderId}/{productionStepId}/{departmentId}")]
+        [Route("productionOrder/{productionOrderId}/productionStep/{productionStepId}/department/{departmentId}")]
         public async Task<ProductionAssignmentModel> GetProductionAssignment([FromRoute] long productionOrderId, [FromRoute] long productionStepId, [FromRoute] int departmentId)
         {
             return await _productionAssignmentService.GetProductionAssignment(productionOrderId, productionStepId, departmentId);
         }
 
         [HttpPut]
-        [Route("{productionStepId}/{productionOrderId}")]
-        public async Task<bool> UpdateProductionAssignment([FromRoute] long productionStepId, [FromRoute] long productionOrderId, [FromBody] ProductionAssignmentInputModel data)
+        [Route("productionOrder/{productionOrderId}/productionStep/{productionStepId}")]
+        public async Task<bool> UpdateProductionAssignment([FromRoute] long productionOrderId, [FromRoute] long productionStepId, [FromBody] ProductionAssignmentInputModel data)
         {
             if (data == null) throw new BadRequestException(GeneralCode.InvalidParams);
-            return await _productionAssignmentService.UpdateProductionAssignment(productionStepId, productionOrderId, data.ProductionAssignments, data.ProductionStepWorkInfo, data.DepartmentTimeTable);
+            return await _productionAssignmentService.UpdateProductionAssignment(productionOrderId, productionStepId, data.ProductionAssignments, data.ProductionStepWorkInfo, data.DepartmentTimeTable);
         }
 
         [HttpPut]
-        [Route("{productionOrderId}")]
+        [Route("productionOrder/{productionOrderId}")]
         public async Task<bool> UpdateProductionAssignment([FromRoute] long productionOrderId, [FromBody] GeneralAssignmentModel data)
         {
             if (data == null) throw new BadRequestException(GeneralCode.InvalidParams);
@@ -61,7 +61,7 @@ namespace VErpApi.Controllers.Manufacturing
         }
 
         [HttpPut]
-        [Route("{productionOrderId}/{productionStepId}/{departmentId}/status/{status}")]
+        [Route("productionOrder/{productionOrderId}/productionStep/{productionStepId}/department/{departmentId}/status/{status}")]
         public async Task<bool> ChangeAssignedProgressStatus([FromRoute] long productionOrderId, [FromRoute] long productionStepId, [FromRoute] int departmentId, EnumAssignedProgressStatus status)
         {
             return await _productionAssignmentService.ChangeAssignedProgressStatus(productionOrderId, productionStepId, departmentId, status);
@@ -75,7 +75,7 @@ namespace VErpApi.Controllers.Manufacturing
         }
 
         [HttpGet]
-        [Route("Departments/{departmentId}")]
+        [Route("departments/{departmentId}")]
         public async Task<PageData<DepartmentProductionAssignmentModel>> DepartmentProductionAssignment([FromRoute] int departmentId, [FromQuery] long? productionOrderId, [FromQuery] int page, [FromQuery] int size, [FromQuery] string orderByFieldName, [FromQuery] bool asc)
         {
             return await _productionAssignmentService.DepartmentProductionAssignment(departmentId, productionOrderId, page, size, orderByFieldName, asc);
@@ -96,14 +96,14 @@ namespace VErpApi.Controllers.Manufacturing
         }
 
         [HttpGet]
-        [Route("{productionOrderId}/capacity/{productionStepId}")]
+        [Route("capacity/productionOrder/{productionOrderId}/productionStep/{productionStepId}")]
         public async Task<CapacityOutputModel> GetCapacityDepartments([FromRoute] long productionOrderId, [FromRoute] long productionStepId, [FromQuery] long startDate, [FromQuery] long endDate)
         {
             return await _productionAssignmentService.GetCapacityDepartments(productionOrderId, productionStepId, startDate, endDate);
         }
 
         [HttpGet]
-        [Route("{productionOrderId}/capacity/general")]
+        [Route("capacity/general/productionOrder/{productionOrderId}")]
         public async Task<CapacityOutputModel> GetGenaralCapacityDepartments([FromRoute] long productionOrderId)
         {
             return await _productionAssignmentService.GetGeneralCapacityDepartments(productionOrderId);
@@ -117,7 +117,7 @@ namespace VErpApi.Controllers.Manufacturing
         }
 
         [HttpGet]
-        [Route("{productionOrderId}/WorkInfo")]
+        [Route("WorkInfo/productionOrder/{productionOrderId}")]
         public async Task<IList<ProductionStepWorkInfoOutputModel>> GetListProductionStepWorkInfo([FromRoute] long productionOrderId)
         {
             return await _productionAssignmentService.GetListProductionStepWorkInfo(productionOrderId);
