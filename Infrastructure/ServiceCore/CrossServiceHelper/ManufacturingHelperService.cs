b1e720245cb2edb524d070e3a4c0744d87346fcf
Tuan Nguyen <minhtuan.titzero@gmail.com>
2021-04-22 11:48:05 +0700

    VW-1293 Bug/Fix export product materials consumption

---
+++ Infrastructure/ServiceCore/CrossServiceHelper/ManufacturingHelperService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VErp.Commons.GlobalObject.InternalDataInterface;
+using VErp.Infrastructure.AppSettings.Model;
+using VErp.Infrastructure.ServiceCore.Service;
+
+namespace VErp.Infrastructure.ServiceCore.CrossServiceHelper
+{
+    public interface IManufacturingHelperService
+    {
+        Task<IList<StepSimpleInfo>> GetStepByArrayId(int[] arrayId);
+    }
+    public class ManufacturingHelperService: IManufacturingHelperService
+    {
+        private readonly IHttpCrossService _httpCrossService;
+        private readonly AppSetting _appSetting;
+        private readonly ILogger _logger;
+
+        public ManufacturingHelperService(IHttpCrossService httpCrossService,
+            IOptions<AppSetting> appSetting,
+            ILogger<ManufacturingHelperService> logger)
+        {
+            _httpCrossService = httpCrossService;
+            _appSetting = appSetting.Value;
+            _logger = logger;
+        }
+
+        public async Task<IList<StepSimpleInfo>> GetStepByArrayId(int[] arrayId)
+        {
+            return await _httpCrossService.Post<IList<StepSimpleInfo>>($"api/internal/InternalManufacturing/steps/array", arrayId);
+        }
+    }
+}
