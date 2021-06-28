84613a04cda90bbd3209065f43e875b4a9f105e0
Tuan Nguyen <minhtuan.titzero@gmail.com>
2021-06-28 11:01:29 +0700

    VW-1449 Bug/Fix references call api GetList Product

--- WebApis/VErpApi/Controllers/Stock/Internal/InternalProductController.cs
+++ WebApis/VErpApi/Controllers/Stock/Internal/InternalProductController.cs
@@ -35,9 +35,9 @@ namespace VErpApi.Controllers.Stock.Internal
         [HttpPost]
         [VErpAction(EnumActionType.View)]
         [Route("")]
-        public async Task<PageData<ProductListOutput>> Search([FromBody] Clause filters, [FromQuery] string keyword, [FromQuery] IList<int> productIds, [FromQuery] string productName, [FromQuery] int page, [FromQuery] int size, [FromQuery] int[] productTypeIds = null, [FromQuery] int[] productCateIds = null, [FromQuery] bool? isProductSemi = null, [FromQuery] bool? isProduct = null)
+        public async Task<PageData<ProductListOutput>> Search([FromBody] Clause filters, [FromQuery] string keyword, [FromQuery] IList<int> productIds, [FromQuery] string productName, [FromQuery] int page, [FromQuery] int size, [FromQuery] int[] productTypeIds = null, [FromQuery] int[] productCateIds = null, [FromQuery] bool? isProductSemi = null, [FromQuery] bool? isProduct = null, [FromQuery] bool? isMaterials = null)
         {
-            return await _productService.GetList(keyword, productIds, productName, productTypeIds, productCateIds, page, size, isProductSemi, isProduct, filters);
+            return await _productService.GetList(keyword, productIds, productName, productTypeIds, productCateIds, page, size, isProductSemi, isProduct, isMaterials, filters);
         }
 
 
