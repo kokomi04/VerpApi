755e10409eb4a4af3f3b1bb105e58c8353cfd9c0
thanhluanbka <thanhluanbka@gmail.com>
2021-07-09 18:09:29 +0700

    VW-1482 Add file

---
+++ Services/Stock/Services.Stock.Model/Product/PropertyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VErp.Commons.GlobalObject;
+using VErp.Infrastructure.EF.StockDB;
+using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+
+namespace VErp.Services.Stock.Model.Product
+{
+    public class PropertyModel: IMapFrom<Property>
+    {
+        public int PropertyId { get; set; }
+        public string PropertyName { get; set; }
+    }
+}
