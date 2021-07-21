2c7457809aff372e0bba5383737e37f7e9aaf43e
thanhluanbka <thanhluanbka@gmail.com>
2021-07-21 10:36:51 +0700

    VW-1475 Fix bug save propertyCalc

--- Infrastructure/EntityFramework/PurchaseOrderDB/PropertyCalcProperty.cs
+++ Infrastructure/EntityFramework/PurchaseOrderDB/PropertyCalcProperty.cs
@@ -6,7 +6,7 @@ namespace VErp.Infrastructure.EF.PurchaseOrderDB
     public partial class PropertyCalcProperty
     {
         public long PropertyCalcId { get; set; }
-        public int PropertypId { get; set; }
+        public int PropertyId { get; set; }
 
         public virtual PropertyCalc PropertyCalc { get; set; }
     }
