e6729ad3d8e1317d86ca91ee3056399dda3034f1
thanhluanbka <thanhluanbka@gmail.com>
2021-08-07 10:40:04 +0700

    VW-1537 CRUD API StockTake and update production plan

---
+++ Services/Stock/Services.Stock.Model/StockTake/StockTakeDetailModel.cs
@@ -0,0 +1,15 @@
+﻿using VErp.Commons.GlobalObject;
+using VErp.Infrastructure.EF.StockDB;
+
+namespace VErp.Services.Stock.Model.StockTake
+{
+    public class StockTakeDetailModel : IMapFrom<StockTakeDetail>
+    {
+        public long StockTakeDetailId { get; set; }
+        public long StockTakeId { get; set; }
+        public int ProductId { get; set; }
+        public int PackageId { get; set; }
+        public decimal StockTakeQuantity { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ Services/Stock/Services.Stock.Service/StockTake/IStockTakeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VErp.Commons.Enums.StandardEnum;
+using VErp.Commons.GlobalObject.InternalDataInterface;
+using VErp.Infrastructure.EF.EFExtensions;
+using VErp.Infrastructure.ServiceCore.Model;
+using VErp.Services.Stock.Model.Inventory;
+using VErp.Services.Stock.Model.StockTake;
+
+namespace VErp.Services.Stock.Service.StockTake
+
+{
+    public interface IStockTakeService
+    {
+        Task<PageData<StockTakePeriotListModel>> GetStockTakePeriods(string keyword, int page, int size);
+
+
+    }
+}
