85b72dcac23872bf30aee476eb08168f062254f4
Tuan Nguyen <minhtuan.titzero@gmail.com>
2021-08-11 11:49:05 +0700

    VW-1569 Bug/Fix CRUD service purchase order excess

---
+++ Services.PurchaseOrder.Service/Po/Implement/PurchaseOrderExcessService.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using VErp.Commons.Enums.ErrorCodes.PO;
+using VErp.Commons.GlobalObject;
+using VErp.Infrastructure.AppSettings.Model;
+using VErp.Infrastructure.EF.PurchaseOrderDB;
+using VErp.Infrastructure.ServiceCore.Service;
+using VErp.Services.PurchaseOrder.Model;
+
+namespace VErp.Services.PurchaseOrder.Service.Implement
+{
+    public class PurchaseOrderExcessService : IPurchaseOrderExcessService
+    {
+        private readonly PurchaseOrderDBContext _purchaseOrderDBContext;
+        private readonly AppSetting _appSetting;
+        private readonly ILogger _logger;
+        private readonly IActivityLogService _activityLogService;
+        private readonly IMapper _mapper;
+
+        public PurchaseOrderExcessService(
+            PurchaseOrderDBContext purchaseOrderDBContext,
+            AppSetting appSetting,
+            ILogger logger,
+            IActivityLogService activityLogService,
+            IMapper mapper)
+        {
+            _purchaseOrderDBContext = purchaseOrderDBContext;
+            _appSetting = appSetting;
+            _logger = logger;
+            _activityLogService = activityLogService;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> UpdatePurchaseOrderExcess(long purchaseOrderExcessId, PurchaseOrderExcessModel model)
+        {
+            var excess = await _purchaseOrderDBContext.PurchaseOrderExcess.FirstOrDefaultAsync(x => x.PurchaseOrderExcessId == purchaseOrderExcessId);
+            if (excess == null)
+                throw new BadRequestException(PurchaseOrderErrorCode.ExcessNotFound);
+
+            _mapper.Map(model, excess);
+            _purchaseOrderDBContext.SaveChanges();
+            return true;
+        }
+    }
+}
