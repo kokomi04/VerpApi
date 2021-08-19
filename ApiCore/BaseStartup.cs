1219311ec6dd8e144f83648260a16b78df3aa163
trungvt <votrongtrung@gmail.com>
2021-08-19 16:22:37 +0700

    VW-1548 Sync api endpoints and buttons interval

--- ApiCore/BaseStartup.cs
+++ ApiCore/BaseStartup.cs
@@ -33,6 +33,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.IO;
 using System.Text.Json;
 using VErp.Commons.Library;
+using VErp.Infrastructure.ApiCore.BackgroundTasks;
 using VErp.Infrastructure.ApiCore.Extensions;
 using VErp.Infrastructure.ApiCore.Filters;
 using VErp.Infrastructure.ApiCore.Middleware;
@@ -90,6 +91,8 @@ namespace VErp.Infrastructure.ApiCore
                   EncryptionAlgorithm = EncryptionAlgorithm.AES_256_GCM,
                   ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
               });
+
+            services.AddHostedService<SyncApiEndpointService>();
 
             services.AddControllers(options =>
             {
