df0b2a1f112df3894742063b81d4c64317d16725
thanhluanbka <thanhluanbka@gmail.com>
2021-10-04 15:00:54 +0700

    VW-1538 Fix calendar

---
+++ Commons/Enums/Organization/EnumDayOffType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace VErp.Commons.Enums.Organization
+{
+    public enum EnumDayOffType
+    {
+        Weekend = 1,
+        DayOff = 2
+    }
+}
