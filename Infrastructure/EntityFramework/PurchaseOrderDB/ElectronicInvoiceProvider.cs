bcd40608af79f71db7884648d347c8cda3da96ee
trungvt <votrongtrung@gmail.com>
2021-10-13 19:38:52 +0700

    VW-1726 Fix convert json fo einvoice config

--- Infrastructure/EntityFramework/PurchaseOrderDB/ElectronicInvoiceProvider.cs
+++ Infrastructure/EntityFramework/PurchaseOrderDB/ElectronicInvoiceProvider.cs
@@ -17,7 +17,7 @@ namespace VErp.Infrastructure.EF.PurchaseOrderDB
         public string ContactName { get; set; }
         public string Address { get; set; }
         public string Description { get; set; }
-        public int? ElectronicInvoiceProviderStatusId { get; set; }
+        public int ElectronicInvoiceProviderStatusId { get; set; }
         public string ConnectionConfig { get; set; }
         public string FieldsConfig { get; set; }
         public int UpdatedByUserId { get; set; }
