2ac983f63ce7b431ac7d92375ed5a88b471bae45
trungvt <votrongtrung@gmail.com>
2022-02-10 17:11:47 +0700

    VW-2009 CRUD package custom property apis

---
+++ WebApis/VErpApi/Controllers/Stock/Stocks/PackageCustomPropertyController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VErp.Infrastructure.ApiCore;
+using VErp.Infrastructure.ApiCore.Model;
+using VErp.Infrastructure.ServiceCore.Model;
+using VErp.Services.Stock.Model.Package;
+using VErp.Services.Stock.Service.Stock;
+
+namespace VErpApi.Controllers.Stock.package
+{
+    [Route("api/stock/PackageCustomProperty")]
+
+    public class PackageCustomPropertyController : VErpBaseController
+    {
+        private readonly IPackageCustomPropertyService _packageCustomPropertyService;
+
+        public PackageCustomPropertyController(IPackageCustomPropertyService packageCustomPropertyService)
+        {
+            _packageCustomPropertyService = packageCustomPropertyService;
+        }
+
+        [HttpGet]
+        public Task<IList<PackageCustomPropertyModel>> Get()
+        {
+            return _packageCustomPropertyService.Get();
+        }
+
+        [HttpGet("{packageCustomPropertyId}")]
+        public Task<PackageCustomPropertyModel> Info([FromRoute] int packageCustomPropertyId)
+        {
+            return _packageCustomPropertyService.Info(packageCustomPropertyId);
+
+        }
+
+        [HttpPost]
+        public Task<int> Create([FromBody] PackageCustomPropertyModel model)
+        {
+            return _packageCustomPropertyService.Create(model);
+        }
+
+        [HttpPut("{packageCustomPropertyId}")]
+        public Task<bool> Update([FromRoute] int packageCustomPropertyId, [FromBody] PackageCustomPropertyModel model)
+        {
+            return _packageCustomPropertyService.Update(packageCustomPropertyId, model);
+        }
+
+        [HttpDelete("{packageCustomPropertyId}")]
+        public Task<bool> Delete([FromRoute] int packageCustomPropertyId)
+        {
+            return _packageCustomPropertyService.Delete(packageCustomPropertyId);
+        }
+
+    }
+}
