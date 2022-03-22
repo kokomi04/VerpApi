f553ad3d6ae6f06ef8e9e3eac1e0de1851ff291d
Tuan Nguyen <minhtuan.titzero@gmail.com>
2022-03-22 10:20:03 +0700

    VW-2056 Bug/Fix additional field into table OvertimeLevel

--- Services/Organization/Services.Organization.Model/TimeKeeping/OvertimeLevelModel.cs
+++ Services/Organization/Services.Organization.Model/TimeKeeping/OvertimeLevelModel.cs
@@ -8,5 +8,7 @@ namespace Services.Organization.Model.TimeKeeping
         public int OvertimeLevelId { get; set; }
         public int OrdinalNumber { get; set; }
         public decimal OvertimeRate { get; set; }
+        public string Title { get; set; }
+        public string Note { get; set; }
     }
 }
