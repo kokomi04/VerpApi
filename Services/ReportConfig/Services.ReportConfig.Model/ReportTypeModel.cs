bf8315f515c6ce6524a375b8449a6ba1569cae6b
trungvt <votrongtrung@gmail.com>
2022-04-20 20:00:26 +0700

    VW-2168 Fix DisplayConfig type to ReportTypeDisplayConfigModel

--- Services/ReportConfig/Services.ReportConfig.Model/ReportTypeModel.cs
+++ Services/ReportConfig/Services.ReportConfig.Model/ReportTypeModel.cs
@@ -55,7 +55,7 @@ namespace Verp.Services.ReportConfig.Model
         public long? TemplateFileId { get; set; }
         public string GroupTitleSql { get; set; }
         public bool? IsDbPaging { get; set; }
-        public string DisplayConfig { get; set; }
+        public ReportDisplayConfigModel DisplayConfig { get; set; }
 
         public IList<ReportColumnModel> Columns { get; set; }
         public bool IsBsc { get; set; }
