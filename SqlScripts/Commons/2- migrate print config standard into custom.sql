1b555b5d399daec522d2eadc8290580b5c54bc0d
trungvt <votrongtrung@gmail.com>
2023-01-04 14:51:50 +0700

    VW-2594 Update migrate script print config

--- SqlScripts/Commons/2- migrate print config standard into custom.sql
+++ SqlScripts/Commons/2- migrate print config standard into custom.sql
@@ -2,8 +2,13 @@ USE MasterDB
 GO
 BEGIN TRY
   BEGIN TRAN
+	DECLARE @tblNewCustomId TABLE
+	(
+		PrintConfigCustomId INT
+	);
 
-    INSERT INTO MasterDB.dbo.PrintConfigCustom (PrintConfigStandardId,
+    INSERT INTO MasterDB.dbo.PrintConfigCustom (
+		PrintConfigStandardId,
         PrintConfigName,
         Title,
         BodyTable,
@@ -26,9 +31,12 @@ BEGIN TRY
         UpdatedDatetimeUtc,
         IsDeleted,
         DeletedDatetimeUtc,
-        SubsidiaryId,
-        ModuleTypeId)
-      SELECT
+        SubsidiaryId--,
+        --ModuleTypeId
+	 )
+	 OUTPUT Inserted.PrintConfigCustomId
+	 INTO @tblNewCustomId(PrintConfigCustomId)
+     SELECT
         v.PrintConfigStandardId,
         v.PrintConfigName,
         v.Title,
@@ -52,44 +60,60 @@ BEGIN TRY
         v.UpdatedDatetimeUtc,
         v.IsDeleted,
         v.DeletedDatetimeUtc,
-        v.SubsidiaryId,
-        v.ModuleTypeId
-      FROM (SELECT
-        p.PrintConfigStandardId,
-        p.PrintConfigName,
-        p.Title,
-        p.BodyTable,
-        p.GenerateCode,
-        p.PaperSize,
-        p.Layout,
-        p.HeadTable,
-        p.FootTable,
-        p.StickyFootTable,
-        p.StickyHeadTable,
-        p.HasTable,
-        p.Background,
-        p.GenerateToString,
-        p.TemplateFilePath,
-        p.TemplateFileName,
-        p.ContentType,
-        p.CreatedByUserId,
-        p.CreatedDatetimeUtc,
-        p.UpdatedByUserId,
-        p.UpdatedDatetimeUtc,
-        p.IsDeleted,
-        p.DeletedDatetimeUtc,
-        s.SubsidiaryId,
-        p.ModuleTypeId
-      FROM MasterDB.dbo.PrintConfigStandard p
-      CROSS APPLY (SELECT
-        s.SubsidiaryId
-      FROM OrganizationDB.dbo.Subsidiary s
-      WHERE s.IsDeleted = 0) s
-      WHERE p.IsDeleted = 0) v
+        v.SubsidiaryId--,
+        --v.ModuleTypeId
+      FROM (
+		SELECT
+			p.PrintConfigStandardId,
+			p.PrintConfigName,
+			p.Title,
+			p.BodyTable,
+			p.GenerateCode,
+			p.PaperSize,
+			p.Layout,
+			p.HeadTable,
+			p.FootTable,
+			p.StickyFootTable,
+			p.StickyHeadTable,
+			p.HasTable,
+			p.Background,
+			p.GenerateToString,
+			p.TemplateFilePath,
+			p.TemplateFileName,
+			p.ContentType,
+			p.CreatedByUserId,
+			p.CreatedDatetimeUtc,
+			p.UpdatedByUserId,
+			p.UpdatedDatetimeUtc,
+			p.IsDeleted,
+			p.DeletedDatetimeUtc,
+			s.SubsidiaryId--,
+			--p.ModuleTypeId
+		  FROM MasterDB.dbo.PrintConfigStandard p
+			  CROSS APPLY (
+				SELECT
+					s.SubsidiaryId
+				  FROM OrganizationDB.dbo.Subsidiary s
+				  WHERE s.IsDeleted = 0
+			  ) s
+		  WHERE p.IsDeleted = 0
+	  ) v
       LEFT JOIN MasterDB.dbo.PrintConfigCustom c
         ON c.PrintConfigStandardId = v.PrintConfigStandardId and v.SubsidiaryId = c.SubsidiaryId and c.IsDeleted = 0
       WHERE c.PrintConfigCustomId is null
+	  
 
+	  INSERT INTO dbo.PrintConfigCustomModuleType
+	  (
+	      PrintConfigCustomId,
+	      ModuleTypeId
+	  )
+	 SELECT c.PrintConfigCustomId, m.ModuleTypeId 
+	 FROM @tblNewCustomId n
+	 JOIN dbo.PrintConfigCustom c ON c.PrintConfigCustomId = n.PrintConfigCustomId
+	 JOIN dbo.PrintConfigStandard s ON c.PrintConfigStandardId = s.PrintConfigStandardId
+	 JOIN dbo.PrintConfigStandardModuleType m ON m.PrintConfigStandardId = s.PrintConfigStandardId;
+	 
   COMMIT TRAN -- Transaction Success!
 END TRY
 BEGIN CATCH
