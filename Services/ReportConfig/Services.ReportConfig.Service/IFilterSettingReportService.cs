554868a9d98f3fa0f325d808c3fd48b189559f44
trungvt <votrongtrung@gmail.com>
2023-04-26 17:28:09 +0700

    VW-2749 Save Report setting and add report setting data as sql query parammeters

---
+++ Services/ReportConfig/Services.ReportConfig.Service/IFilterSettingReportService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verp.Services.ReportConfig.Service
+{
+    public interface IFilterSettingReportService
+    {
+        Task<bool> Update(int reportTypeId, Dictionary<int, object> fieldValues);
+    }
+}
