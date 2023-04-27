fbfecee3479f05a3b65b23f51323b5adc8898671
trungvt <votrongtrung@gmail.com>
2023-04-27 18:04:35 +0700

    VW-2748 Move Organization apis to another folder out of System folder

---
+++ WebApis/VErpApi/Controllers/Organization/Internal/Salary/InternalSalaryPeriodAdditionTypeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VErp.Commons.GlobalObject.InternalDataInterface.Hr.Salary;
+using VErp.Infrastructure.ApiCore;
+using VErp.Infrastructure.ServiceCore.CrossServiceHelper;
+using VErp.Services.Organization.Model.Salary;
+using VErp.Services.Organization.Service.HrConfig;
+using VErp.Services.Organization.Service.Salary;
+
+namespace VErpApi.Controllers.Organization.Internal.Salary
+{
+
+
+    [Route("api/internal/[controller]")]
+    public class InternalSalaryPeriodAdditionTypeController : CrossServiceBaseController
+    {
+        public readonly ISalaryPeriodAdditionTypeService _salaryPeriodAdditionTypeService;
+
+        public InternalSalaryPeriodAdditionTypeController(ISalaryPeriodAdditionTypeService salaryPeriodAdditionTypeService)
+        {
+            _salaryPeriodAdditionTypeService = salaryPeriodAdditionTypeService;
+        }
+
+        [HttpGet("")]
+        public async Task<IEnumerable<ISalaryPeriodAddtionTypeBase>> GetList()
+        {
+            return await _salaryPeriodAdditionTypeService.List();
+        }
+    }
+}
---
+++ WebApis/VErpApi/Controllers/Organization/Salary/SalaryEmployeeController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VErp.Commons.Enums.MasterEnum;
+using VErp.Commons.GlobalObject;
+using VErp.Infrastructure.ApiCore;
+using VErp.Infrastructure.ApiCore.Attributes;
+using VErp.Services.Organization.Model.Salary;
+using VErp.Services.Organization.Service.Salary;
+
+namespace VErpApi.Controllers.Organization.Salary
+{
+    [Route("api/organization/salary/employee")]
+    public class SalaryEmployeeController : VErpBaseController
+    {
+        private readonly ISalaryEmployeeService _salaryEmployeeService;
+
+        public SalaryEmployeeController(ISalaryEmployeeService salaryEmployeeService)
+        {
+            _salaryEmployeeService = salaryEmployeeService;
+        }
+
+
+        [HttpGet("warnings")]
+        public async Task<GroupSalaryEmployeeWarningInfo> GetSalaryGroupEmployeesWarning()
+        {
+            return await _salaryEmployeeService.GetSalaryGroupEmployeesWarning();
+        }
+
+
+
+        [HttpPost("periods/{salaryPeriodId}/groups/{salaryGroupId}/Eval")]
+        [VErpAction(EnumActionType.View)]
+        public async Task<IList<NonCamelCaseDictionary<SalaryEmployeeValueModel>>> EvalSalaryEmployeeByGroup([FromRoute] int salaryPeriodId, [FromRoute] int salaryGroupId, [FromBody] GroupSalaryEmployeeModel req)
+        {
+            return await _salaryEmployeeService.EvalSalaryEmployeeByGroup(salaryPeriodId, salaryGroupId, req);
+        }
+
+        [HttpGet("periods/{salaryPeriodId}/groups/{salaryGroupId}/GetData")]
+        public async Task<IList<NonCamelCaseDictionary<SalaryEmployeeValueModel>>> GetSalaryEmployeeByGroup([FromRoute] int salaryPeriodId, [FromRoute] int salaryGroupId)
+        {
+            return await _salaryEmployeeService.GetSalaryEmployeeByGroup(salaryPeriodId, salaryGroupId);
+        }
+
+        [HttpGet("periods/{salaryPeriodId}/AllData")]
+        public async Task<IList<GroupSalaryEmployeeEvalData>> GetSalaryEmployeeAll([FromRoute] int salaryPeriodId)
+        {
+            return await _salaryEmployeeService.GetSalaryEmployeeAll(salaryPeriodId);
+        }
+
+
+        [HttpPut("periods/{salaryPeriodId}/groups/{salaryGroupId}/UpdateData")]
+        public async Task<bool> Update([FromRoute] int salaryPeriodId, [FromRoute] int salaryGroupId, [FromBody] GroupSalaryEmployeeModel req)
+        {
+            return await _salaryEmployeeService.Update(salaryPeriodId, salaryGroupId, req);
+        }
+    }
+}
