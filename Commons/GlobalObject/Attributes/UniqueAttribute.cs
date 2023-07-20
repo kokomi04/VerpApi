cf6a7dd103b5fd645a387b4ccb11c625bd62a67b
phamduy0404 <duypt.netdev@gmail.com>
2023-07-20 17:40:36 +0700

    VW-2898 Validate AbsenceTypeSymbol model

---
+++ Commons/GlobalObject/Attributes/UniqueAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VErp.Commons.GlobalObject.Attributes
+{
+    public class UniqueAttribute<TContext, TEntity> : ValidationAttribute 
+        where TContext : DbContext
+        where TEntity : class
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+                return ValidationResult.Success;
+
+            var dbContext = (TContext)validationContext.GetService(typeof(TContext));
+
+            var entityType = validationContext.ObjectType;
+            var propertyName = validationContext.MemberName;
+            var propertyValue = value.ToString();
+
+            var set = dbContext.Set<TEntity>();
+
+            if (set.Any(x => EF.Property<string>(x, propertyName) == propertyValue))
+                return new ValidationResult(ErrorMessage);
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
--- Services/Organization/Services.Organization.Model/TimeKeeping/AbsenceTypeSymbolModel.cs
+++ Services/Organization/Services.Organization.Model/TimeKeeping/AbsenceTypeSymbolModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using VErp.Commons.GlobalObject;
+using VErp.Commons.GlobalObject.Attributes;
 using VErp.Infrastructure.EF.OrganizationDB;
 
 namespace Services.Organization.Model.TimeKeeping
@@ -8,6 +9,8 @@ namespace Services.Organization.Model.TimeKeeping
     {
         public int AbsenceTypeSymbolId { get; set; }
         public int NumericalOrder { get; set; }
+
+        [MaxLength(200, ErrorMessage = "Mô tả không được vượt quá 200 ký tự")]
         public string TypeSymbolDescription { get; set; }
 
         [Required(ErrorMessage = "Vui lòng nhập số ngày nghỉ tối đa/tháng")]
@@ -16,9 +19,12 @@ namespace Services.Organization.Model.TimeKeeping
 
         [Required(ErrorMessage = "Vui lòng nhập ký hiệu loại vắng")]
         [MaxLength(20, ErrorMessage = "Ký hiệu loại vắng quá dài")]
+        [Unique<OrganizationDBContext, AbsenceTypeSymbol>(ErrorMessage = "Ký hiệu loại vắng đã tồn tại")]
         public string SymbolCode { get; set; }
         public bool IsUsed { get; set; }
         public bool IsCounted { get; set; }
+
+        [Range(0, 1, ErrorMessage = "Tỷ lệ hưởng lương phải nằm trong khoảng từ 0 đến 1")]
         public double SalaryRate { get; set; }
         public bool IsDefaultSystem { get; set; }
     }
