49dbdf4f4dacb863d3955afdd97e185ed53fa975
phamduy0404 <duypt.netdev@gmail.com>
2023-07-21 13:22:44 +0700

    VW-2899 Refector OvertimeLevelModel

--- Services/Organization/Services.Organization.Model/TimeKeeping/OvertimeLevelModel.cs
+++ Services/Organization/Services.Organization.Model/TimeKeeping/OvertimeLevelModel.cs
@@ -1,4 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
 using VErp.Commons.GlobalObject;
+using VErp.Commons.GlobalObject.Attributes;
+using VErp.Commons.GlobalObject.DataAnnotationsExtensions;
 using VErp.Infrastructure.EF.OrganizationDB;
 
 namespace Services.Organization.Model.TimeKeeping
@@ -6,9 +9,20 @@ namespace Services.Organization.Model.TimeKeeping
     public class OvertimeLevelModel : IMapFrom<OvertimeLevel>
     {
         public int OvertimeLevelId { get; set; }
-        public int OrdinalNumber { get; set; }
+        public int NumericalOrder { get; set; }
+
+        [Min(1, ErrorMessage = "Tỷ lệ hưởng lương phải lớn hơn hoặc bằng 1")]
         public decimal OvertimeRate { get; set; }
-        public string Title { get; set; }
-        public string Note { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập ký hiệu mức tăng ca")]
+        [MaxLength(20, ErrorMessage = "Ký hiệu mức tăng ca quá dài")]
+        [Unique<OrganizationDBContext, OvertimeLevel>(ErrorMessage = "Ký hiệu mức tăng ca đã tồn tại")]
+        public string OvertimeCode { get; set; }
+
+        [MaxLength(200, ErrorMessage = "Mô tả không được vượt quá 200 ký tự")]
+        [Required(ErrorMessage = "Vui lòng nhập mô tả mức tăng ca")]
+        public string Description { get; set; }
+
+        public int OvertimePriority { get; set; }
     }
 }
