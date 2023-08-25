a5cc9146a24b5a0a80709d4aa4d9304c790f76dd
trungvt <votrongtrung@gmail.com>
2023-08-25 14:07:52 +0700

    hotfix activity log json serialize inventory detail => req

--- Services/Stock/Services.Stock.Service/Inventory/Implement/InventoryProcess/InventoryBillInputService.AffectUpdate.cs
+++ Services/Stock/Services.Stock.Service/Inventory/Implement/InventoryProcess/InventoryBillInputService.AffectUpdate.cs
@@ -891,7 +891,7 @@ namespace VErp.Services.Stock.Service.Stock.Implement
                     await _packageActivityLog.LogBuilder(() => InventoryBillInputActivityLogMessage.UpdatedCauseByRefInvInput)
                      .MessageResourceFormatDatas(packageInfo.PackageCode, req.Inventory.InventoryCode)
                      .ObjectId(packageInfo.PackageId)
-                     .JsonData(packageInfo.JsonSerialize())
+                     .JsonData(req.JsonSerialize())
                      .CreateLog();
                 }
 
@@ -938,7 +938,7 @@ namespace VErp.Services.Stock.Service.Stock.Implement
                     await builder
                        .MessageResourceFormatDatas(inv.InventoryCode, req.Inventory.InventoryCode)
                        .ObjectId(inv.InventoryId)
-                       .JsonData(invDetails.JsonSerialize())
+                       .JsonData(req.JsonSerialize())
                        .CreateLog();
                 }
 
