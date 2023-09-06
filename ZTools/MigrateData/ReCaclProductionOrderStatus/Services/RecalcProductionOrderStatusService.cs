83cdeea7e822d098bcc5ae9882bfef3f73be1837
trungvt <votrongtrung@gmail.com>
2023-09-06 11:40:26 +0700

    VW-2860-2 Create migrate production order status and fix pending status when manual finish

---
+++ ZTools/MigrateData/ReCaclProductionOrderStatus/Services/RecalcProductionOrderStatusService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VErp.Commons.GlobalObject.QueueMessage;
+using VErp.Infrastructure.EF.ManufacturingDB;
+using VErp.Services.Manafacturing.Service.ProductionProcess;
+
+namespace ReCaclProductionOrderStatus.Services
+{
+    public interface IRecalcProductionOrderStatusService
+    {
+        Task Execute();
+    }
+
+    public class RecalcProductionOrderStatusService : IRecalcProductionOrderStatusService
+    {
+        private readonly IMigrateInventoryService migrateInventoryService;
+        private readonly IProductionProgressService productionProgressService;
+        private readonly ManufacturingDBContext manufacturingDBContext;
+
+        public RecalcProductionOrderStatusService(IProductionProgressService productionProgressService, IMigrateInventoryService migrateInventoryService, ManufacturingDBContext manufacturingDBContext)
+        {
+            this.productionProgressService = productionProgressService;
+            this.migrateInventoryService = migrateInventoryService;
+            this.manufacturingDBContext = manufacturingDBContext;
+        }
+
+        public async Task Execute()
+        {
+            var productionOrderCodes = await manufacturingDBContext.ProductionOrder.Select(o => o.ProductionOrderCode).ToListAsync();
+            foreach (var code in productionOrderCodes)
+            {
+                var data = await migrateInventoryService.GetProductionOrderCalcStatusV2Message(new ProductionOrderStatusInventorySumaryMessage()
+                {
+                    ProductionOrderCode = code,
+                    Description = "Auto migrate"
+                });
+
+                await productionProgressService.CalcAndUpdateProductionOrderStatusV2(data);
+            }
+        }
+    }
+}
