bdd5098af71e85240c71c9bb84bb8873e4a74ec3
baobn1998 <baobn1998@gmail.com>
2023-10-20 14:09:08 +0700

    VW-3044: Input program function to filter

--- Infrastructure/ServiceCore/CrossServiceHelper/System/ProgramingFunctionHelperService.cs
+++ Infrastructure/ServiceCore/CrossServiceHelper/System/ProgramingFunctionHelperService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using VErp.Commons.GlobalObject;
 using VErp.Commons.GlobalObject.InternalDataInterface.System;
 using VErp.Infrastructure.ServiceCore.Service;
 
@@ -10,6 +12,7 @@ namespace VErp.Infrastructure.ServiceCore.CrossServiceHelper.System
     {
         Task<IList<ProgramingFunctionBaseModel>> Sqls();
         Task<IList<ProgramingFunctionBaseModel>> UserSqls();
+        Task<IList<ProgramingFunctionBaseModel>> GetAllSqls();
     }
     public class ProgramingFunctionHelperService : IProgramingFunctionHelperService
     {
@@ -27,6 +30,19 @@ namespace VErp.Infrastructure.ServiceCore.CrossServiceHelper.System
         public async Task<IList<ProgramingFunctionBaseModel>> UserSqls()
         {
             return await _httpCrossService.Get<List<ProgramingFunctionBaseModel>>($"api/internal/InternalUserProgramingFunction", new { });
+        }
+        public async Task<IList<ProgramingFunctionBaseModel>> GetAllSqls()
+        {
+            var sqlFunctions = await _httpCrossService.Get<List<ProgramingFunctionBaseModel>>($"api/internal/InternalProgramingFunction/Sqls", new { });
+            var userSqlFunctions = await _httpCrossService.Get<List<ProgramingFunctionBaseModel>>($"api/internal/InternalUserProgramingFunction", new { });
+            foreach (var function in userSqlFunctions)
+            {
+                var sqlFunction = sqlFunctions.FirstOrDefault(x => x.ProgramingFunctionName == function.ProgramingFunctionName);
+                if (sqlFunction != null)
+                    sqlFunctions.Remove(sqlFunction);
+                sqlFunctions.Add(function);
+            }
+            return sqlFunctions;
         }
     }
 }
