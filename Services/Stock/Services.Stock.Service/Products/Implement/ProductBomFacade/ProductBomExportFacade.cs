6b1bfceef7f96194584c52de43c38a0b9bf5801e
baobn1998 <baobn1998@gmail.com>
2023-11-09 16:15:13 +0700

    VW-3088: Change startColumnIndex

--- Services/Stock/Services.Stock.Service/Products/Implement/ProductBomFacade/ProductBomExportFacade.cs
+++ Services/Stock/Services.Stock.Service/Products/Implement/ProductBomFacade/ProductBomExportFacade.cs
@@ -25,7 +25,7 @@ namespace VErp.Services.Stock.Service.Products.Implement.ProductBomFacade
         private ISheet sheet = null;
         private int currentRow = 0;
 
-        const int START_PROP_COLUMN_INDEX = 18;
+        const int START_PROP_COLUMN_INDEX = 19;
 
         private int maxColumnIndex;
 
