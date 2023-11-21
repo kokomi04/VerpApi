e77e5a1f576929f75b8ae9e62af5c96c5d2d06cd
trungvt <votrongtrung@gmail.com>
2023-11-21 17:18:44 +0700

    VW-3082 Store assign by date by each output of production step

--- Infrastructure/EntityFramework/ManufacturingDB/ProductionAssignment.cs
+++ Infrastructure/EntityFramework/ManufacturingDB/ProductionAssignment.cs
@@ -41,7 +41,7 @@ public partial class ProductionAssignment
 
     public decimal RateInPercent { get; set; }
 
-    public bool IsUseMinAssignHours { get; set; }
+    public bool? IsUseMinAssignHours { get; set; }
 
     public virtual ICollection<ProductionAssignmentDetail> ProductionAssignmentDetail { get; set; } = new List<ProductionAssignmentDetail>();
 
---
+++ Infrastructure/EntityFramework/ManufacturingDB/ProductionAssignmentDetailLinkData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VErp.Infrastructure.EF.ManufacturingDB;
+
+public partial class ProductionAssignmentDetailLinkData
+{
+    public long ProductionOrderId { get; set; }
+
+    public long ProductionStepId { get; set; }
+
+    public int DepartmentId { get; set; }
+
+    public DateTime WorkDate { get; set; }
+
+    public long ProductionStepLinkDataId { get; set; }
+
+    public decimal QuantityPerDay { get; set; }
+
+    public decimal WorkloadPerDay { get; set; }
+
+    public decimal HoursPerDay { get; set; }
+
+    public decimal MinAssignHours { get; set; }
+
+    public bool IsUseMinAssignHours { get; set; }
+
+    public virtual ProductionAssignmentDetail ProductionAssignmentDetail { get; set; }
+}
